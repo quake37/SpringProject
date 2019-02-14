aa4b8db95f62e9b5f009289f525feb646b0d10a3
gangnamKGITBANK <47413715+gangnamKGITBANK@users.noreply.github.com>
2019-02-14 17:37:37 +0900

    terst

--- StudyDamoyeo/src/main/java/com/StudyDamoyeo/domain/MemberVO.java
+++ StudyDamoyeo/src/main/java/com/StudyDamoyeo/domain/MemberVO.java
@@ -1,7 +1,5 @@
 package com.StudyDamoyeo.domain;
 
-
-
 import java.util.Date;
 
 import lombok.Data;
@@ -10,12 +8,73 @@ import lombok.Setter;
 
 @Data
 public class MemberVO {
-	private int mno;
+
 	private String id;
 	private String pw;
 	private String nickname;
 	private String memberType;
 	private String profile_Img;
+	private String phone;
+	private String email;
 	private Date regDate;
 	private Date uDate;
+	public String getId() {
+		return id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getProfile_Img() {
+		return profile_Img;
+	}
+	public void setProfile_Img(String profile_Img) {
+		this.profile_Img = profile_Img;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getMemberType() {
+		return memberType;
+	}
+	public void setMemberType(String memberType) {
+		this.memberType = memberType;
+	}
+	
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public Date getuDate() {
+		return uDate;
+	}
+	public void setuDate(Date uDate) {
+		this.uDate = uDate;
+	}
+	
 }
--- StudyDamoyeo/src/test/java/com/StudyDamoyeo/mapper/MemberServiceTest.java
+++ StudyDamoyeo/src/test/java/com/StudyDamoyeo/mapper/MemberServiceTest.java
@@ -1,5 +1,6 @@
 package com.StudyDamoyeo.mapper;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.StudyDamoyeo.domain.MemberVO;
+import com.StudyDamoyeo.service.MemberService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -15,24 +17,46 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class MemberServiceTest {
 	@Autowired
-	private MemberMapper mapper;
-
-	/*
-	 * @Test public void testGetList() { mapper.getList().forEach(member ->
-	 * member.getClass()); }
-	 * 
-	 * @Test public void testInsert() { MemberVO vo = new MemberVO();
-	 * 
-	 * 
-	 * }
-	 * 
-	 * @Test public void testRead() { MemberVO vo = service.read("test");
-	 * log.info(vo); }
-	 * 
-	 * @Test public void testDelete() { service.delete("test"); }
-	 * 
-	 * @Test public void testUpdate() { MemberVO vo = new MemberVO();
-	 * 
-	 * service.update(vo); log.info(vo); }
-	 */
+	private MemberService service;
+	
+	@Test
+	@Ignore
+	public void testInsert() {
+		MemberVO vo = new MemberVO();
+		vo.setId("test");
+		vo.setMemberType("0");
+		vo.setNickname("test");
+		vo.setProfile_Img("default");
+		vo.setPw("test");
+		vo.setPhone("test");
+		vo.setEmail("test@test.com");
+		service.insert(vo);
+	}
+	@Test
+	@Ignore
+	public void testRead() {
+		service.read("test");
+	}
+	@Test
+	@Ignore
+	public void testUpdate() {
+		MemberVO vo = new MemberVO();
+		vo.setId("test");
+		vo.setMemberType("0");
+		vo.setNickname("test");
+		vo.setProfile_Img("profile");
+		vo.setPw("test");
+		vo.setPhone("test");
+		vo.setEmail("test@test.com");
+		service.update(vo);
+	}
+	@Test
+	@Ignore
+	public void testSelect() {
+		service.getList();
+	}
+	@Test
+	public void testDelete() {
+		service.delete("test");
+	}
 }
