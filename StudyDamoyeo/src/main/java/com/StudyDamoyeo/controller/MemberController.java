bda353707ac17d5cc4e6524c3c2d4d74aa96de70
user <user@DESKTOP-3BD0KIF>
2019-02-18 20:11:11 +0900

    1

--- StudyDamoyeo/src/main/java/com/StudyDamoyeo/controller/MemberController.java
+++ StudyDamoyeo/src/main/java/com/StudyDamoyeo/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.StudyDamoyeo.domain.MemberVO;
@@ -50,6 +51,7 @@ public class MemberController {
 	
 
 	@PostMapping("/idcheck")
+	@ResponseBody
 	public Map<Object, Object> idcheck(@RequestBody String userid) {
 		int count = 0;
 		Map<Object, Object> map = new HashMap<Object, Object>();
