dc7b4b81678350045102e87688c4720b9c50c067
user <user@DESKTOP-3BD0KIF>
2019-02-18 23:42:51 +0900

    1

--- StudyDamoyeo/src/main/webapp/resources/main/assets/js/register.js
+++ StudyDamoyeo/src/main/webapp/resources/main/assets/js/register.js
@@ -1,49 +1,60 @@
 var token = $("meta[name='_csrf']").attr("content");
 var header = $("meta[name='_csrf_header']").attr("content");
 
-$(document).ajaxSend(function(e, xhr, options) {
-    xhr.setRequestHeader(header, token);
-});
 
 $(document).ready(function() {
 	 var idReg = /^[a-z0-9]{6,20}$/g;
 	 var regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-+<>@\#$%&\\\=\(\'\"]/gi;
-	
-	
-	$("#RegisterComId").on("input", function() {
-		var userid = $("#RegisterComId").val();
+	 var userid;
+	 var timeout;
+	 var delay = 2000;   // 2 seconds
+	$("#RegisterComId").on("change keyup paste", function() {
+		userid = $("#RegisterComId").val();
 		
 		if(!idReg.test(userid)){
+			$("#comIdCheck").css({"color":"#f54f29"});
 			$("#comIdCheck2").text("if문 : "+idReg.test(userid));
 			$("#comIdCheck").text("6~20자 영문자 또는 숫자이어야 합니다.");
 			$("#comIdCheck").show(500);
 			$("#RegisterComId").focus();
 		}else{
 			$("#comIdCheck2").text("else문 : "+idReg.test(userid));
-			$("#comIdCheck").hide();
-			alert(userid);
-			  $.ajax({
-		            async: true,
-		            type : 'POST',
-		            data : userid,
-		            url : "/member/idcheck",
-		            dataType : "json",
-		            contentType: "application/json; charset=UTF-8",
-		            success : function(data) {
-		            	if (data.cnt > 0) {
-		                    alert("아이디가 존재합니다. 다른 아이디를 입력해주세요.");
-		                } else {
-		                    alert("사용가능한 아이디입니다.");                    
-		                }  	
+			$("#comIdCheck").hide(500);
+			$("#RegisterComSpin").show(500);
+			if(timeout) {
+		       clearTimeout(timeout);
+		    }
+		    timeout = setTimeout(function() {
+		    	 $.ajax({
+			            async: true,
+			            type : 'POST',
+			            data : userid,
+			            url : "/member/idcheck",
+			            dataType : "json",
+			            contentType: "application/json",
+			            beforeSend : function(xhr)
+		            {   /*데이터를 전송하기 전에 헤더에 csrf값을 설정한다*/
+		                xhr.setRequestHeader(header, token);
 		            },
-		            error : function(request,status,error) {	                
-		            	  alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+			            success : function(data) {
+			            	if (data.cnt > 0) {
+			                    alert("아이디가 존재합니다. 다른 아이디를 입력해주세요.");
+			                } else {
+			                	$("#RegisterComSpin").hide(500);
+			                	$("#comIdCheck").css({"color":"blue"});
+			                	$("#comIdCheck").text("사용가능한 아이디입니다.");
+			                	$("#comIdCheck").show(500);
+			                }  	
+			            },
+			            error : function(request,status,error) {	                
+			            	  alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
 
-		            }
-			  });
+			            }
+				  });
+		    }, delay);
+			 
 		}
 	});
-	
 	
 	
 });
