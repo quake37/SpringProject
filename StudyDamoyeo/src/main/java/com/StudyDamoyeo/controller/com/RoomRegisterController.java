e95ba1d1ee7cc3fe2d035d80e9bf78a1dd152964
user <user@DESKTOP-3BD0KIF>
2019-02-22 10:47:48 +0900

    1

--- StudyDamoyeo/src/main/java/com/StudyDamoyeo/controller/com/RoomRegisterController.java
+++ StudyDamoyeo/src/main/java/com/StudyDamoyeo/controller/com/RoomRegisterController.java
@@ -1,6 +1,9 @@
 package com.StudyDamoyeo.controller.com;
 
+import java.io.File;
 import java.security.Principal;
+
+import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -9,8 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.StudyDamoyeo.domain.MemberVO;
+import com.StudyDamoyeo.domain.RoomVO;
 import com.StudyDamoyeo.service.RoomService;
 
 import lombok.extern.log4j.Log4j;
@@ -22,6 +27,8 @@ public class RoomRegisterController {
 
 	@Autowired
 	RoomService service;
+	@Autowired
+	ServletContext application;
 
 	@GetMapping("/register")
 	public String roomRegister() {return "/com/roomRegister";}
@@ -30,9 +37,16 @@ public class RoomRegisterController {
 	@GetMapping("/location")
 	public String goRegister() {return "/com/locationRegister";}
 	
-	@PostMapping("/insertRoom")
-	public String insertRoom(MemberVO vo, Model model) {
+	@PostMapping(value ="/insertRoom",headers = ("content-type=multipart/*"))
+	public String insertRoom(RoomVO vo, Model model,MultipartFile[] imgname, Principal principal) {
+		String uploadFolder = application.getRealPath("/resources/upload");
+		String uploadFolderPath = principal.getName();
+		// make folder --------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
 		
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
 		
 		return null;
 		
